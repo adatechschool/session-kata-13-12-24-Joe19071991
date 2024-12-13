dc5fdb755ae7b3107c1880ed21a8079487d318d4
Joe19071991 <lauranove66@gmail.com>
2024-12-13 12:14:51 +0100

    modification et test de la fonction isValidGuess

--- script.js
+++ script.js
@@ -8,13 +8,19 @@ function isValidGuess(guess) {
   if (guess.length !== secretCombinaison.length) {
     return false;
   }
-  return guess.every((color) => colors.includes(color));
+
+  for (let i = 0; i < guess.length; i++) {
+    if (!colors.includes(guess[i])) {
+      return false;
+    }
+  }
+  return true;
 }
 
 // Test de la fonction isValidGuess
-// console.log(isValidGuess(["rouge", "bleu"])); // true
-// console.log(isValidGuess(["rouge", "noir"])); // false
-// console.log(isValidGuess(["rouge"])); // false
+console.log(isValidGuess(["rouge", "bleu"])); // true
+console.log(isValidGuess(["rouge", "noir"])); // false
+console.log(isValidGuess(["rouge"])); // false
 
 function isCorrectCombinaison(guess) {
   for (let i = 0; i < guess.length; i++) {
@@ -25,5 +31,5 @@ function isCorrectCombinaison(guess) {
   return true;
 }
 
-console.log(isCorrectCombinaison(["rouge", "bleu"])); // true
-console.log(isCorrectCombinaison(["bleu", "rouge"])); // false
+//console.log(isCorrectCombinaison(["rouge", "bleu"])); // true
+//console.log(isCorrectCombinaison(["bleu", "rouge"])); // false
